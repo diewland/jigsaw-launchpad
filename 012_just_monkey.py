d6c73545ad5d2a229d634f7dec5dec82e99d3d21
diewland <diewland@gmail.com>
2022-10-01 01:46:10 +0700

    add just-monkey (draft)

---
+++ 012_just_monkey.py
@@ -0,0 +1,36 @@
+import json
+ 
+TOKEN_SIZE = 10 # TODO confirm ***
+START_ID = 0
+OUTPUT_DIR = '012_just_monkey'
+
+NAME = "JustMonkey 🐵"
+DESC = "No Utility No Roadmap No Reveal(Maybe)  Just Monkey 🐵 I love Banananana Uga Uga !!!"
+IMG = "https://diewland.github.io/jigsaw-launchpad/assets/just-monkey/Monkey.gif"
+ATTRS = [
+    # standard
+    #{
+    #  "trait_type": "Collection",
+    #  "value": NAME, 
+    #},
+    # custom
+    #{
+    #  "trait_type": "Rarity",
+    #  "value": "Unrevealed",
+    #},
+]
+ENGINE = "Jigsaw Engine"
+
+metadata = {
+  "name": "***",
+  "description": DESC,
+  "image": "***",
+  "attributes": ATTRS,
+  "compiler": ENGINE,
+}
+
+for id in range(0, TOKEN_SIZE):
+    metadata["name"] = "{} #{}".format(NAME, id)
+    metadata["image"] = IMG#.format(id)
+    with open("./{}/{}.json".format(OUTPUT_DIR, START_ID + id), "w") as f:
+        json.dump(metadata, f, ensure_ascii=False)
