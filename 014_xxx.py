c2739af77b08b8e71b8702c1150f95074ed0f453
diewland <diewland@gmail.com>
2022-10-01 20:36:21 +0700

    add xxx metadata

---
+++ 014_xxx.py
@@ -0,0 +1,36 @@
+import json
+ 
+TOKEN_SIZE = 169
+START_ID = 0
+OUTPUT_DIR = '014_xxx'
+
+NAME = "WWY"
+DESC = "Where were you?"
+IMG = "https://diewland.github.io/jigsaw-launchpad/assets/xxx/{}.png" #0-168
+ATTRS = [
+    # standard
+    #{
+    #  "trait_type": "Collection",
+    #  "value": NAME, 
+    #},
+    # custom
+    #{
+    #  "trait_type": "Rarity",
+    #  "value": "Unrevealed",
+    #},
+]
+ENGINE = "Jigsaw Engine"
+
+metadata = {
+  "name": "***",
+  "description": DESC,
+  "image": "***",
+  "attributes": ATTRS,
+  "compiler": ENGINE,
+}
+
+for id in range(0, TOKEN_SIZE):
+    metadata["name"] = "{} #{}".format(NAME, id)
+    metadata["image"] = IMG.format(id)
+    with open("./{}/{}.json".format(OUTPUT_DIR, START_ID + id), "w") as f:
+        json.dump(metadata, f, ensure_ascii=False)
